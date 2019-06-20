a64e3adf40a0b80379edf3a83a505914dee9d7db
ryan <2869888186@qq.com>
2019-06-20 12:19:09 +0800

    add hompage

---
+++ build/precache-manifest.dfde50caec5c5ff87203604fdd59f654.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148911bd2768fa5f820bca339a73172f",
+    "url": "/xgame/build/index.html"
+  },
+  {
+    "revision": "5e092403903c397c1133",
+    "url": "/xgame/build/static/css/main.a8ea3c65.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/xgame/build/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "5e092403903c397c1133",
+    "url": "/xgame/build/static/js/main.dfd85f73.chunk.js"
+  },
+  {
+    "revision": "d85cc76a3391142f98c9",
+    "url": "/xgame/build/static/js/runtime~main.0f352a41.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b16f17bef28e7cb93281c5e5f8e259d.js"
+  "/xgame/build/precache-manifest.dfde50caec5c5ff87203604fdd59f654.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/xgame/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
